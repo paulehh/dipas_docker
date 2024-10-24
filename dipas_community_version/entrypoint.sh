d728bec72222ec9861f6bf6ad80464e1358e1af3
matlendzi <87185599+matlendzi@users.noreply.github.com>
2024-10-25 00:33:34 +0200

    Update entrypoint.sh
    fixed file permission issues

--- dipas_community_version/entrypoint.sh
+++ dipas_community_version/entrypoint.sh
@@ -18,7 +18,7 @@ if PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -d "${DB_NAM
         && chmod -R 750 /var/www/html/dipas/htdocs/drupal \
         && chmod 444 /var/www/html/dipas/htdocs/drupal/.htaccess \
         && chmod 440 /var/www/html/dipas/htdocs/drupal/sites/default/settings.* \
-        && chmod 440 /var/www/html/dipas/config/drupal.services.yml \
+        && chmod 644 /var/www/html/dipas/config/drupal.services.yml \
         && chmod -R 775 /var/www/html/dipas/htdocs/drupal/sites/default/files \
         && find /var/www/html/dipas/htdocs/drupal/sites/default/files -type f -exec chmod 664 {} \;
     exec "$@"  # Execute the original command
@@ -41,22 +41,22 @@ else
         && chmod -R 750 /var/www/html/dipas/htdocs/drupal \
         && chmod 444 /var/www/html/dipas/htdocs/drupal/.htaccess \
         && chmod 440 /var/www/html/dipas/htdocs/drupal/sites/default/settings.* \
-        && chmod 440 /var/www/html/dipas/config/drupal.services.yml \
+        && chmod 644 /var/www/html/dipas/config/drupal.services.yml \
         && chmod -R 775 /var/www/html/dipas/htdocs/drupal/sites/default/files \
         && find /var/www/html/dipas/htdocs/drupal/sites/default/files -type f -exec chmod 664 {} \;
 
     # Run Drush installation and other necessary commands
     cd /var/www/html/dipas/htdocs
-    ./vendor/bin/drush site-install --db-url=pgsql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME} \
+    su www-data -s /bin/bash -c "vendor/bin/drush site-install --db-url=pgsql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME} \
     --account-name=${DRUPAL_ADMIN_USER} --account-pass=${DRUPAL_ADMIN_PASS} \
-    --site-name="${DRUPAL_SITE_NAME}" --yes --existing-config
+    --site-name='${DRUPAL_SITE_NAME}' --yes --existing-config"
 
     # Set ignored config entities to avoid issues with imported configurations
-    ./vendor/bin/drush config-set config_ignore.settings ignored_config_entities '' -y
+    su www-data -s /bin/bash -c "vendor/bin/drush config-set config_ignore.settings ignored_config_entities '' -y"
 
     # Import configuration and rebuild caches
-    ./vendor/bin/drush config-import -y
-    ./vendor/bin/drush cache-rebuild
+    su www-data -s /bin/bash -c "vendor/bin/drush config-import -y"
+    su www-data -s /bin/bash -c "vendor/bin/drush cache-rebuild"
 fi
 
 # Execute the main CMD (start Apache)
