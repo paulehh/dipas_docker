c053117ea8ca9ec59adddf32a9d5a64b4524bdee
matlendzi <87185599+matlendzi@users.noreply.github.com>
2024-10-24 23:57:38 +0200

    Update entrypoint.sh

--- dipas_community_version/entrypoint.sh
+++ dipas_community_version/entrypoint.sh
@@ -4,6 +4,23 @@
 if PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -d "${DB_NAME}" -c "SELECT 1 FROM users_field_data WHERE name = '${DRUPAL_ADMIN_USER}';" | grep -q "1"; then
     echo "Admin user '${DRUPAL_ADMIN_USER}' exists."
     echo "Skipping Drupal installation."
+    # Substitute environment variables into settings.php and drupal.services.yml
+    envsubst '${DB_NAME} ${DB_USER} ${DB_PASSWORD} ${DB_HOST} ${DB_PORT} ${DRUPAL_SITE_DOMAIN}' < /var/www/html/dipas/htdocs/drupal/sites/default/settings.php > /var/www/html/dipas/htdocs/drupal/sites/default/settings.tmp.php
+    envsubst '${DRUPAL_SITE_DOMAIN}' < /var/www/html/dipas/config/drupal.services.yml > /var/www/html/dipas/config/drupal.services.tmp.yml
+    mv /var/www/html/dipas/htdocs/drupal/sites/default/settings.tmp.php /var/www/html/dipas/htdocs/drupal/sites/default/settings.php
+    mv /var/www/html/dipas/config/drupal.services.tmp.yml /var/www/html/dipas/config/drupal.services.yml
+
+    # Create temporary directory for Drupal
+    mkdir -p /var/www/html/tmp && chown www-data:www-data /var/www/html/tmp && chmod 777 /var/www/html/tmp
+
+    # Set correct file permissions
+    chown -R www-data:www-data /var/www/html/dipas/htdocs/drupal \
+        && chmod -R 750 /var/www/html/dipas/htdocs/drupal \
+        && chmod 444 /var/www/html/dipas/htdocs/drupal/.htaccess \
+        && chmod 440 /var/www/html/dipas/htdocs/drupal/sites/default/settings.* \
+        && chmod 440 /var/www/html/dipas/config/drupal.services.yml \
+        && chmod -R 775 /var/www/html/dipas/htdocs/drupal/sites/default/files \
+        && find /var/www/html/dipas/htdocs/drupal/sites/default/files -type f -exec chmod 664 {} \;
     exec "$@"  # Execute the original command
 else
     echo "Admin user '${DRUPAL_ADMIN_USER}' does not exist. Proceeding with setup..."
