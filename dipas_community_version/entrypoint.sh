644ebdc0df0c2bacac3ec7af0cdff47ecb100cdc
Mateusz Lendzinski <87185599+matlendzi@users.noreply.github.com>
2024-10-28 19:42:15 +0100

    Update entrypoint.sh

--- dipas_community_version/entrypoint.sh
+++ dipas_community_version/entrypoint.sh
@@ -51,7 +51,7 @@ else
     --site-name='${DRUPAL_SITE_NAME}' --yes --existing-config"
 
     # Import translation files
-    su www-data -s /bin/bash -c "vendor/bin/drush locale:import de /var/www/html/dipas/config/de.po --type=not-customized --overwrite"
+    su www-data -s /bin/bash -c "vendor/bin/drush locale:import de /var/www/html/dipas/config/de.po --type=not-customized"
     su www-data -s /bin/bash -c "vendor/bin/drush locale:import de /var/www/html/dipas/htdocs/drupal/modules/custom/dipas_stories/files/translations/dipas_stories.de.po --type=not-customized"
     su www-data -s /bin/bash -c "vendor/bin/drush locale:import de /var/www/html/dipas/htdocs/drupal/modules/custom/dipas/files/translations/dipas.de.po --type=not-customized"
     su www-data -s /bin/bash -c "vendor/bin/drush locale:import de /var/www/html/dipas/htdocs/drupal/modules/custom/masterportal/files/translations/masterportal.de.po --type=not-customized"
